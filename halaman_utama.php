0aabbe111576cbd5608be577d95b066916812912
putrilarasati <putrilarasatilaras@gmail.com>
2015-03-10 22:53:08 +0700

    halaman utama
    baru template

---
+++ halaman_utama.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<title>Citrus Island</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" href="images/CitrusIsland.css" type="text/css" />
+</head>
+<body>
+<div id="wrap">
+  <div id="header">
+    <form method="post" class="search" action="#">
+      <p>
+        <input name="search_query" class="textbox" type="text" />
+        <input name="search" class="button" value="Search" type="submit" />
+      </p>
+    </form>
+    <h1 id="logo">Explore<span>Surabaya</span></h1>
+    <h2 id="slogan">Surabaya Tourism Information</h2>
+  </div>
+  <div id="menu">
+    <ul>
+      <li id="current"><a href="#">Beranda</a></li>
+      <li><a href="#">Daftar Wisata</a></li>
+      <li><a href="#">Event and News</a></li>
+      
+    </ul>
+  </div>
+  <div id="sidebar" >
+    <h1>Jenis Wisata</h1>
+    <ul class="sidemenu">
+      <li><a href="#">Musium</a></li>
+      <li><a href="#TemplateInfo">Taman</a></li>
+      <li><a href="#SampleTags">Wisata Alam</a></li>
+      <li><a href="#">Taman Rekreasi</a></li>
+      
+    </ul>
+    <h1>Harga</h1>
+    <ul class="sidemenu">
+      <li><a href="#">>20.000,-</a></li>
+      <li><a href="#">20.000-50.000</a></li>
+      <li><a href="#">50.000-100.000</a></li>
+      <li><a href="#">>100.000</a></li>
+    </ul>
+    <h1>Lokasi</h1>
+	      <li><a href="#">Surabaya Pusat</a></li>
+		  <li><a href="#">Surabaya Tengah</a></li>
+		  <li><a href="#">Surabaya Timur</a></li>
+		  <li><a href="#">Surabaya Selatan</a></li>
+		  <li><a href="#">Surabaya Barat</a></li>
+    
+  </div>
+  <div id="main"> <a name="MostViewed"></a>
+    <h1>Most Viewed</h1>
+    Kenjeran Park
+    <p>Good luck and I hope you find my free templates useful!</p>
+    <p class="post-footer align-right"> <a href="#" class="readmore">Read more</a> <a href="#" class="comments">Comments ()</a> <span class="date">Oct 11, 2006</span> </p>
+    
+    <br />
+  </div>
+  <div id="rightbar">
+    <h1>Event and News</h1>
+    <p>Glow Run</p>
+    <h1>Rekomendasi Editor</h1>
+    <p>Museum Bank Indonesia </p>
+  </div>
+</div>
+<div id="footer">
+  <div id="footer-content">
+ 
+    <div id="footer-left"> contact us </div>
+  </div>
+</div>
+</body>
+</html>
